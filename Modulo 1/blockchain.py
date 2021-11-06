49262180451f07e96f2ae58e630a423323202b12
FernandoCalleRodriguez <60648315+FernandoCalleRodriguez@users.noreply.github.com>
2021-11-06 16:59:14 +0100

    añadidos métodos de validacion y de proof_of_work

--- Modulo 1/blockchain.py
+++ Modulo 1/blockchain.py
@@ -32,6 +32,36 @@ class Blockchain:
     def get_previous_block(self):
         return self.chain[-1]
     
+    def proof_of_work(self, previous_proof):
+        new_proof = 1
+        check_proof = False
+        while check_proof is False:
+            hash_operation = hashlib.sha256(str(new_proof**2-previous_proof**2).encode()).hexdigest()
+            if hash_operation[:4] == "0000":
+                check_proof = True
+            else:
+                new_proof += 1
+        return new_proof
+
+    def hash(self, block):
+        encode_block = json.dump(block, sort_keys = True).encode()
+        return hashlib.sha256(encode_block).hexdigest()
+    
+    def is_chain_valid(self, chain):
+        previous_block = chain[0]
+        block_index = 1
+        while block_index < len(chain):
+            block = chain[block_index]
+            if block['previous_hash'] != self.hash(previous_block):
+                return False
+            previous_proof = previous_block['proof']
+            proof = block['proof']
+            hash_operation = hashlib.sha256(str(proof**2-previous_proof**2).encode()).hexdigest()
+            if hash_operation[:4] != "0000":
+                return False
+            previous_block = block
+            block_index += 1
+        return True
+                 
 #Parte 2 - Minado de un Bloque de la Cadena
 
-        
