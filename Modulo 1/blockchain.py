d1131922966e320d16083d454423b25a1fbb2452
FernandoCalleRodriguez <60648315+FernandoCalleRodriguez@users.noreply.github.com>
2021-11-06 12:49:57 +0100

    Creado metodo init, create_block y get_previous_block

--- Modulo 1/blockchain.py
+++ Modulo 1/blockchain.py
@@ -14,6 +14,24 @@ import json
 from flask import Flask, jsonify
 
 #Parte 1 - Crear la Cadena de Bloques
-
-
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof = 1, previous_hash = '0')
+        
+    def create_block(self, proof, previous_hash):
+        block = {
+            'index' : len(self.chain)+1,
+            'timestamp' : str(datetime.datetime.now()),
+            'proof' : proof,
+            'previous_hash' : previous_hash
+            }
+        self.chain.append(block)
+        return block
+    
+    def get_previous_block(self):
+        return self.chain[-1]
+    
 #Parte 2 - Minado de un Bloque de la Cadena
+
+        
