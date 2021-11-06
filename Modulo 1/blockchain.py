30a0673ef4588ba19750bf7fda60ba368f4dc562
FernandoCalleRodriguez <60648315+FernandoCalleRodriguez@users.noreply.github.com>
2021-11-06 12:24:18 +0100

    importadas las librerias que se van a usar

--- Modulo 1/blockchain.py
+++ Modulo 1/blockchain.py
@@ -6,3 +6,14 @@ Created on Sat Nov  6 12:06:55 2021
 """
 
 #Módulo 1: Crear un Cadena de Bloques
+
+# Importar las librerias
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
+
+#Parte 1 - Crear la Cadena de Bloques
+
+
+#Parte 2 - Minado de un Bloque de la Cadena
